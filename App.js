599f84c9e5b3aa589aa37d47dd40febc700ee78a
stephenwayar <stephenbuluswayar@gmail.com>
2022-03-11 20:48:07 +0100

    event listerner and input components

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [name, setName] = useState("Steve")
+  const [age, setAge] = useState(21)
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter your name:</Text>
+
+      <TextInput 
+        style={styles.input}
+        multiline
+        placeholder='Enter your name'
+        onChangeText={(value) => setName(value)}
+      />
+
+      <Text>Enter your age:</Text>
+
+      <TextInput 
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder='Enter your age'
+        onChangeText={(value) => setAge(value)}
+      />
+
+      <Text>Your name is {name}</Text>
+      <Text>Your age is {age}</Text>
     </View>
   );
 }
@@ -13,8 +34,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 8,
+    marginVertical: 5
+  }
 });
